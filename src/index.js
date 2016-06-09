4f4e17bd9bda9f8445da5994ee6eaa239f91f46c
Seray Uzgur <seray.uzgur@mebitech.com>
2016-06-09 17:10:12 +0300

    clean up

--- src/index.js
+++
@@ -1,5 +0,0 @@
-export { ShallowComponent } from "./components/ShallowComponent";
-export { Arrays } from "./utils/Arrays";
-export { Objects } from "./utils/Objects";
-export { Maps } from "./utils/Maps";
-export { Cookies } from "./utils/Cookies";
