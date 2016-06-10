24f1472745bcc226439966ad272190e02ee774ef
Seray Uzgur <seray.uzgur@mebitech.com>
2016-06-10 13:56:27 +0300

    showcase init

--- src/faicon/FaIcon.jsx
+++ src/faicon/FaIcon.jsx
@@ -6,7 +6,7 @@ import "./font-awesome/4.6.1/css/font-awesome.css";
  * This component wraps font-awesome elements. Also it loads font-awesome.min.css. No need to load it somewhere else.
  * Please look at https://fortawesome.github.io/Font-Awesome/examples/
  */
-class FaIcon extends ShallowComponent {
+export default class FaIcon extends ShallowComponent {
 
     static propTypes = {
         code: React.PropTypes.string.isRequired,
@@ -27,4 +27,3 @@ class FaIcon extends ShallowComponent {
         );
     }
 }
-module.exports = FaIcon;
---
+++ webpack.config.dev.js
@@ -0,0 +1,37 @@
+const webpack = require("webpack");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const CachePlugin = require("webpack/lib/CachePlugin");
+
+
+const commonSettings = require("./webpack.config.common.js");
+
+commonSettings.cache = true;
+commonSettings.debug = true;
+commonSettings.devtool = "eval";
+commonSettings.entry = {
+    app: [commonSettings.paths.app]
+};
+
+commonSettings.devServer = {
+    historyApiFallback: true,
+    hot: true,
+    progress: true,
+    inline: true,
+
+    // display only errors to reduce the amount of output
+    //stats: "errors-only",
+
+    // parse host and port from env so this is easy
+    // to customize
+    // host: process.env.HOST,
+    host: "0.0.0.0",
+    port: 8080
+};
+
+commonSettings.plugins.push(new webpack.HotModuleReplacementPlugin());
+commonSettings.plugins.push(new CopyWebpackPlugin([{ from: "showcase" }]));
+
+commonSettings.plugins.push(new CachePlugin({}));
+
+
+module.exports = commonSettings;
