bf2ffc3434dd6fb8ab8f8d66d2a0dde65653d066
Seray Uzgur <serayuzgur@Serays-MacBook-Pro.local>
2016-06-13 16:06:37 +0300

    PasswordField

--- src/inputs/PasswordInput.jsx
+++ src/inputs/PasswordInput.jsx
@@ -2,27 +2,51 @@ import React from "react";
 import { ShallowComponent } from "robe-react-commons";
 import Input from "inputs/BaseInput";
 
+/**
+ * An input element for passwords.
+ * Password wrapper for BaseInput. 
+ * @export
+ * @class PasswordInput
+ * @extends {ShallowComponent}
+ */
 export default class PasswordInput extends ShallowComponent {
+    /**
+     * propTypes
+     * @static
+     */
     static propTypes = {
         label: React.PropTypes.string,
         value: React.PropTypes.string.isRequired,
         onChange: React.PropTypes.func
     };
 
-    /* eslint no-useless-constructor: 0*/
+    /**
+     * Creates an instance of PasswordInput.
+     * 
+     * @param props (description)
+     */
     constructor(props) {
         super(props);
     }
 
+    /**
+     * render
+     * 
+     * @returns
+     */
     render() {
         return (
             <Input
                 {...this.props}
                 type="password"
                 ref="innerInput"
-            />);
+                />);
     }
 
+    /**
+     * Returns validity of the component.
+     * @return true if it is valid.
+     */
     isValid = () => {
         return this.refs.innerInput.isValid();
     };
