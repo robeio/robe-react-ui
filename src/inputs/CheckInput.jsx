babf068f9cbacb0b6dafbef426fd9983bd163ae1
Seray Uzgur <serayuzgur@Serays-MacBook-Pro.local>
2016-06-13 13:09:36 +0300

    CheckInput

--- src/inputs/CheckInput.jsx
+++ src/inputs/CheckInput.jsx
@@ -7,57 +7,112 @@ const Style = {
         "marginLeft": "-5px",
         "marginRight": "10px"
     },
-    "selected": {},
-    "disabled": {}
+    "disabled": {
+        "pointerEvents": "none",
+        "opacity": "0.4"
+    }
 };
 
-class CheckInput extends ShallowComponent {
+/**
+ * An Input Component which acts as a checkbox.
+ * 
+ * @export
+ * @class CheckInput
+ * @extends {ShallowComponent}
+ */
+export default class CheckInput extends ShallowComponent {
+    /**
+     * propTypes
+     * @static
+     */
     static propTypes = {
         label: React.PropTypes.string,
         textValue: React.PropTypes.string,
-        value: React.PropTypes.bool.isRequired,
+        disabled: React.PropTypes.bool,
+        value: React.PropTypes.bool,
         onChange: React.PropTypes.func
     };
 
+    /**
+     * defaultProps
+     * @static
+     */
     static defaultProps = {
-        disabled: false
+        disabled: false,
+        value: false
     };
 
+    /**
+     * Creates an instance of CheckInput.
+     * 
+     * @param props (description)
+     */
     constructor(props) {
         super(props);
+        this.state = {
+            value: this.props.value | false
+        }
     };
 
-    checked = this.props.value;
 
-
+    /**
+     * render
+     * @returns
+     */
     render() {
-        let icon = this.props.value === true ? "fa-check-circle-o" : "fa-circle-o";
-        let disabled = this.props.disabled ? "checkbox disabled-check-input" : "checkbox ";
+        let icon = "fa-circle-o";
+        console.log(this.props.onChange);
+        if (this.props.onChange == undefined) {
+            if (this.state.value) {
+                icon = "fa-check-circle-o";
+            }
+        } else {
+            if (this.props.value === true) {
+                icon = "fa-check-circle-o";
+            }
+        }
+
+        let isDisabled = this.props.disabled === true;
 
         return (
-            <div className={disabled} onClick={this.__parse.bind(this) }>
+            <div
+                className="checkbox"
+                style={(isDisabled ? Style.disabled : {}) }
+                /**
+                 * (description)
+                 */
+                onClick={(!isDisabled ? this.__onClick : undefined) }>
                 <label style={{ paddingLeft: "2px" }}>
                     <FaIcon code={icon} style={Style.icon} />
                     {this.props.label}</label>
             </div>
         );
+    }
 
-    };
 
+    /**
+     * Returns whether it is selected or not.
+     * @returns true if selected.
+     */
     isChecked = () => {
-        return this.checked;
+        if (this.props.onChange) {
+            return this.prop.value;
+        } else {
+            return this.state.value;
+        }
     };
 
-
-    __parse = (e) => {
+    /**
+     * Internal onClick event. It is triggered every time. 
+     * @param e event
+     */
+    __onClick = (e) => {
         if (this.props.onChange) {
             e.target.parsedValue = !this.props.value;
             e.target.value = this.props.textValue;
             this.props.onChange(e);
-            this.checked = !this.checked;
+        } else {
+            this.setState({ value: !this.state.value });
         }
     };
-
 }
-
-module.exports = CheckInput;
