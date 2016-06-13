2640e6bef0da2622884f4955bdaf7ecbc8bd69d2
Kamil Büküm <kamil.bukum@mebitech.com>
2016-06-13 20:37:02 +0300

    changed reference of Inputs name.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ export { default as CheckBoxTree } from "./checkboxtreelist";
 export { default as DataGrid } from "./datagrid";
 export { default as Faicon } from "./faicon";
 export { default as Form } from "./form";
-export { default as BaseInput } from "./inputs";
+export { default as Input } from "./inputs";
 export { default as Loading } from "./loading";
 export { default as Notification } from "./notification";
 export { default as Progress } from "./progress";
