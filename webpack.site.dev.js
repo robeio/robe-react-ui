8594eddf9e72377e010a2d82dd2cfd2d9252527e
serayuzgur <serayuzgur@gmail.com>
2016-07-22 13:04:11 +0300

    port

--- webpack.site.dev.js
+++ webpack.site.dev.js
@@ -17,6 +17,7 @@ webPackConfig.entry = {
 // webPackConfig.module.preLoaders.push({ test: /.jsx?$/, loader: "eslint", exclude: /node_modules/ });
 
 
+console.log("PORT:", process.env.PORT);
 webPackConfig.devServer = {
     historyApiFallback: true,
     hot: true,
