1996cc53fca49d9f3a324edc5389c851aa959490
serayuzgur <serayuzgur@gmail.com>
2016-08-03 11:17:56 +0300

    Search for Site/Components.

--- site/components/Components.jsx
+++ site/components/Components.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ShallowComponent from "robe-react-commons/lib/components/ShallowComponent";
 import Renderer from "./Renderer";
-import { Grid, Col, ListGroup, ListGroupItem } from "react-bootstrap";
+import { Grid, Col, ListGroup, ListGroupItem, InputGroup } from "react-bootstrap";
 import "react-notifications/lib/notifications.css";
 import NotificationContainer from "react-notifications/lib/NotificationContainer";
 import ComponentList from "./ComponentList";
 import Progress from "progress/Progress";
+import TextInput from "inputs/TextInput";
+import FaIcon from "faicon/FaIcon";
 
 
 
@@ -14,7 +16,8 @@ export default class Showcase extends ShallowComponent {
     constructor(props: Object) {
         super(props);
         this.state = {
-            componentSelection: window.location.hash.substring(1) === "Components" ? "Components/TextInput" : window.location.hash.substring(1)
+            componentSelection: window.location.hash.substring(1) === "Components" ? "Components/TextInput" : window.location.hash.substring(1),
+            filter: ""
         };
     }
 
@@ -22,29 +25,31 @@ export default class Showcase extends ShallowComponent {
         let componentDetail;
         let componentMenu = [];
         let components = ComponentList.getComponentList(this.state, this.__handleChange);
-
+     
         for (let i = 0; i < components.length; i++) {
             let item = components[i];
             let active = this.state.componentSelection === `Components/${item.header}`;
-            componentMenu.push(
-                <ListGroupItem
-                    href={`#Components/${item.header}`}
-                    key={`#${item.header}`}
-                    onClick={this.__onComponenListClick}
-                    active={active}
-                >
-                    {item.header}
-                </ListGroupItem>);
-            if (active) {
-                componentDetail = (
-                    <Renderer
-                        header={item.header}
-                        desc={item.desc}
-                        alternatives={item.alternatives}
-                        json={item.json}
-                        sample={item.sample}
-                        code={item.code}
-                    />);
+            if (item.header.toLowerCase().indexOf(this.state.filter.toLowerCase()) !== -1) {
+                componentMenu.push(
+                    <ListGroupItem
+                        href={`#Components/${item.header}`}
+                        key={`#${item.header}`}
+                        onClick={this.__onComponenListClick}
+                        active={active}
+                    >
+                        {item.header}
+                    </ListGroupItem>);
+                if (active) {
+                    componentDetail = (
+                        <Renderer
+                            header={item.header}
+                            desc={item.desc}
+                            alternatives={item.alternatives}
+                            json={item.json}
+                            sample={item.sample}
+                            code={item.code}
+                        />);
+                }
             }
         }
         return (
@@ -52,13 +57,27 @@ export default class Showcase extends ShallowComponent {
                 <NotificationContainer />
                 <h2>Components</h2>
                 <h5>Here you can find the samples and usages of the components.</h5>
-                <Col xs={12} sm={3} style={{ borderRight: "lightgray 1px solid" }} > <ListGroup>{componentMenu}</ListGroup></Col>
+                <Col xs={12} sm={3} style={{ borderRight: "lightgray 1px solid" }} >
+                    <TextInput
+                        inputGroupRight={<InputGroup.Addon> <FaIcon code="fa-search" size="fa-sm" /> </InputGroup.Addon>}
+                        onChange={this.__onFilterChange}
+                        value={this.state.filter}
+                        placeholder="Search"
+                    />
+                    <ListGroup>{componentMenu}</ListGroup>
+                </Col>
                 <Col xs={12} sm={9} üref="componentView">
                     {componentDetail}
                 </Col>
             </Grid>
         );
     }
+
+    __onFilterChange = (e: Object) => {
+        this.setState({
+            filter: e.target.value
+        });
+    };
 
     __onComponenListClick = (e: Object) => {
         this.setState({
