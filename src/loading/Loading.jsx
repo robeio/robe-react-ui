c63d76f67a23e0dc8d963c64b958a2bb01d854d8
Kamil Büküm <kamil.bukum@mebitech.com>
2016-08-05 14:43:12 +0300

    - loading path problem resolved.

--- src/loading/Loading.jsx
+++ src/loading/Loading.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ShallowComponent } from "robe-react-commons";
-import "loading/style.css";
+import "./style.css";
 
 export default class Loading extends ShallowComponent {
     static style = {
