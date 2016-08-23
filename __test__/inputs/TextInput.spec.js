2b8f0eaad3087aed155a4685a0e990edc0e31ec8
hasanmumin <hasan.mumin@mebitech.com>
2016-08-23 17:41:55 +0300

    TextInput test added.

--- __test__/inputs/TextInput.spec.js
+++ __test__/inputs/TextInput.spec.js
@@ -3,6 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import TestUtils from "react-addons-test-utils";
 import TextInput from "inputs/TextInput";
+import { mount, simulate,shallow,expect } from 'enzyme';
 
 describe("inputs/TextInput", () => {
     const getComponent = (props: Object): Object => {
@@ -37,4 +38,37 @@ describe("inputs/TextInput", () => {
         chai.assert.equal(componentNode.isValid(), false);
         chai.assert.equal(ReactDOM.findDOMNode(componentNode).getElementsByClassName("input-alert").length, 1);
     });
+
+    
+
+    let values = "";
+
+    const handleChange = (e) => {
+    values = e.target.value;};
+
+     it("'OnChange' Control", () => {
+     
+        let wrapper = mount(getComponent({ onChange: handleChange }));
+        chai.assert.equal(wrapper.find(TextInput).length, 1);
+        let textInput = wrapper.find("input").first();
+
+        textInput.simulate('change', {target: {value: 'robe'}});
+
+        chai.assert.equal("robe", values);
+
+        wrapper = mount(getComponent({}));
+
+        wrapper.setProps({onChange:undefined});
+
+        textInput = wrapper.find("input").first();
+
+        values="";
+        textInput.simulate('change', {target: {value: ''}});
+
+        chai.assert.equal("", values);
+
+    });
+
+
+
 });
