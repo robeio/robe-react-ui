5a7fd19abc4e7ffce063cab4935df0eeaf1034f8
hasanmumin <hasan.mumin@mebitech.com>
2016-08-23 18:03:48 +0300

    eslint

--- __test__/inputs/CheckInput.spec.js
+++ __test__/inputs/CheckInput.spec.js
@@ -1,9 +1,7 @@
 import React from "react";
 import chai from "chai";
-import TestUtils from "react-addons-test-utils";
 import CheckInput from "inputs/CheckInput";
-import Generator from "../test-utils/Generator";
-import { mount, simulate,shallow,expect } from 'enzyme';
+import {mount} from "enzyme";
 
 describe("inputs/CheckInput", () => {
     const langs = [
@@ -21,83 +19,74 @@ describe("inputs/CheckInput", () => {
         }
     ];
 
-    const lang= {
-            text: "English",
-            value: "en"
-        };
-
-     const getComponent = (props: Object): Object => {
-        return (
-            <CheckInput {...props} />
-        );
+    const getComponent = (props: Object): Object => {
+        return (<CheckInput {...props} />);
     };
-    
+
 
     let values = [];
 
     const handleChange = (e) => {
-    values = e.target.value;};
+        values = e.target.value;
+    };
 
 
- it("props", () => {
-
+    it("props", () => {
         let props = {
-            items:langs,
-            value:values,
-            label:"Label"
+            items: langs,
+            value: values,
+            label: "Label"
         };
-         let wrapper = mount(getComponent(props));
-         chai.assert.equal(wrapper.props().label,'Label');
-         chai.assert.equal(wrapper.props().disabled,false);
+        let wrapper = mount(getComponent(props));
+        chai.assert.equal(wrapper.props().label, "Label");
+        chai.assert.equal(wrapper.props().disabled, false);
     });
 
 
     it("onChange", () => {
-         let wrapper = mount(getComponent({ items: langs, value: values, onChange: handleChange }));
+        let wrapper = mount(getComponent({items: langs, value: values, onChange: handleChange}));
         chai.assert.equal(wrapper.find(CheckInput).length, 1);
         let checkInput = wrapper.find(".fa-square-o").first();
 
         checkInput.simulate("click");
-        wrapper = mount(getComponent({ items: langs, value: values, onChange: handleChange }));
+        wrapper = mount(getComponent({items: langs, value: values, onChange: handleChange}));
         chai.assert.equal(wrapper.find(".fa-check-square-o").length, 1);
 
         checkInput.simulate("click");
-        wrapper = mount(getComponent({ items: langs, value: values }));
+        wrapper = mount(getComponent({items: langs, value: values}));
         chai.assert.equal(wrapper.find(".fa-check-square-o").length, 1);
-
 
 
     });
 
 
-
     it("isChecked", () => {
-        values=[];
+        values = [];
 
         let wrapper = mount(getComponent({ items: langs, value: values, onChange: handleChange }));
-        chai.assert.equal(wrapper.instance().isChecked("en"),false);
+        chai.assert.equal(wrapper.instance().isChecked("en"), false);
         chai.assert.equal(wrapper.find(CheckInput).length, 1);
         let checkInput = wrapper.find(".fa-square-o").first();
-        chai.assert.equal(wrapper.instance().isChecked(),false);
-        chai.assert.equal(wrapper.instance().isChecked("en"),false);
-        chai.assert.equal(wrapper.instance().isChecked("tr"),false);
-        chai.assert.equal(wrapper.instance().isChecked("kr"),false);
+        chai.assert.equal(wrapper.instance().isChecked(), false);
+        chai.assert.equal(wrapper.instance().isChecked("en"), false);
+        chai.assert.equal(wrapper.instance().isChecked("tr"), false);
+        chai.assert.equal(wrapper.instance().isChecked("kr"), false);
 
         checkInput.simulate("click");
-        wrapper = mount(getComponent({ items: langs, value: values,onChange: handleChange }));
+        wrapper = mount(getComponent({items: langs, value: values, onChange: handleChange}));
 
-        chai.assert.equal(wrapper.instance().isChecked("en"),true);
-        chai.assert.equal(wrapper.instance().isChecked("tr"),false);
-        chai.assert.equal(wrapper.instance().isChecked("kr"),false);
+        chai.assert.equal(wrapper.instance().isChecked("en"), true);
+        chai.assert.equal(wrapper.instance().isChecked("tr"), false);
+        chai.assert.equal(wrapper.instance().isChecked("kr"), false);
 
         checkInput = wrapper.find(".fa-square-o").at(1);
 
         checkInput.simulate("click");
-        wrapper = mount(getComponent({ items: langs, value: values,onChange: handleChange }));
+        wrapper = mount(getComponent({items: langs, value: values, onChange: handleChange}));
 
-        chai.assert.equal(wrapper.instance().isChecked("en"),true);
-        chai.assert.equal(wrapper.instance().isChecked("tr"),false);
-        chai.assert.equal(wrapper.instance().isChecked("kr"),true);
+        chai.assert.equal(wrapper.instance().isChecked("en"), true);
+        chai.assert.equal(wrapper.instance().isChecked("tr"), false);
+        chai.assert.equal(wrapper.instance().isChecked("kr"), true);
 
     });
 });
--- __test__/inputs/RadioInput.spec.js
+++ __test__/inputs/RadioInput.spec.js
@@ -1,126 +1,108 @@
 import React from "react";
 import chai from "chai";
-import TestUtils from "react-addons-test-utils";
 import RadioInput from "inputs/RadioInput";
-import Generator from "../test-utils/Generator";
-import { mount, simulate,shallow } from 'enzyme';
+import {mount} from "enzyme";
 
 describe("inputs/RadioInput", () => {
-
-    const langs = [
-    {
+    const langs = [{
         text: "English",
         value: "en"
-    },
-    {
+    }, {
         text: "Turkish",
         value: "tr"
-    },
-    {
+    }, {
         text: "Kurdish",
         value: "kr"
     }
-];
+    ];
 
 
-     const getComponent = (props: Object): Object => {
+    const getComponent = (props: Object): Object => {
         return (
             <RadioInput {...props} />
         );
     };
-    
+
 
     let values = [];
 
     const handleChange = (e) => {
-    values = e.target.value;};
+        values = e.target.value;
+    };
 
     it("props", () => {
-           let props = {
-            label:"Label",
-            disabled:false,
-            hidden:false
+        let props = {
+            label: "Label",
+            disabled: false,
+            hidden: false
         };
-         let wrapper = mount(getComponent(props));
-         chai.assert.equal(wrapper.props().label,'Label');
-         chai.assert.equal(wrapper.props().disabled,false);
-         chai.assert.equal(wrapper.props().hidden,false);
+        let wrapper = mount(getComponent(props));
+        chai.assert.equal(wrapper.props().label, "Label");
+        chai.assert.equal(wrapper.props().disabled, false);
+        chai.assert.equal(wrapper.props().hidden, false);
 
-         wrapper.setProps({label:"New Label",disabled:true,hidden:true});
-         chai.assert.equal(wrapper.props().hidden,true);
-         chai.assert.equal(wrapper.props().disabled,true);
-         chai.assert.equal(wrapper.props().label,'New Label');
+        wrapper.setProps({label: "New Label", disabled: true, hidden: true});
+        chai.assert.equal(wrapper.props().hidden, true);
+        chai.assert.equal(wrapper.props().disabled, true);
+        chai.assert.equal(wrapper.props().label, "New Label");
     });
 
 
     it("onChange", () => {
-            let props = {
-                label:"Label",
-                disabled:false,
-                hidden:false,
-                items:langs[0]
-            };
-            let wrapper = mount(getComponent({ items: langs, value: values, onChange: handleChange }));
-            let radioInput = wrapper.find(".fa-circle-o").first();
+        let props = {
+            label: "Label",
+            disabled: false,
+            hidden: false,
+            items: langs[0]
+        };
+        let wrapper = mount(getComponent({items: langs, value: values, onChange: handleChange}));
+        let radioInput = wrapper.find(".fa-circle-o").first();
 
-            radioInput.simulate("click");
-            wrapper = mount(getComponent({ items: langs, value: values, onChange: handleChange }));
-            chai.assert.equal(wrapper.find(".fa-dot-circle-o").length, 1);
+        radioInput.simulate("click");
+        wrapper = mount(getComponent({items: langs, value: values, onChange: handleChange}));
+        chai.assert.equal(wrapper.find(".fa-dot-circle-o").length, 1);
 
-        });
-
-
-        it("isChecked", () => {
-
-            values=[];
-            let props = {
-                label:"Label",
-                disabled:false,
-                hidden:false,
-                items:langs
-            };
-            let wrapper = mount(getComponent({ items: langs, value: values, onChange: handleChange }));
-
-            chai.assert.equal(wrapper.instance().isChecked(), false);
-            let radioInput = wrapper.find(".fa-circle-o").first();
-
-            radioInput.simulate("click");
-            wrapper = mount(getComponent({ items: langs, value: values, onChange: handleChange }));
-            chai.assert.equal(wrapper.instance().isChecked(), true);
-
-            radioInput = wrapper.find(".fa-circle-o").last();
-
-            radioInput.simulate("click");
-            wrapper = mount(getComponent({ items: langs, value: values, onChange: handleChange }));
-            chai.assert.equal(wrapper.instance().isChecked("en"), true);
-
-        });
+    });
 
 
-        it("getValue", () => {
+    it("isChecked", () => {
+        values = [];
+        let wrapper = mount(getComponent({items: langs, value: values, onChange: handleChange}));
 
-            values=[];
-            let props = {
-                label:"Label",
-                disabled:false,
-                hidden:false,
-                items:langs
-            };
-            let wrapper = mount(getComponent({ items: langs, value: values, onChange: handleChange }));
+        chai.assert.equal(wrapper.instance().isChecked(), false);
+        let radioInput = wrapper.find(".fa-circle-o").first();
 
-            chai.assert.equal(wrapper.instance().getValue(), "");
-            let radioInput = wrapper.find(".fa-circle-o").first();
+        radioInput.simulate("click");
+        wrapper = mount(getComponent({items: langs, value: values, onChange: handleChange}));
+        chai.assert.equal(wrapper.instance().isChecked(), true);
 
-            radioInput.simulate("click");
-            wrapper = mount(getComponent({ items: langs, value: values }));
-            chai.assert.equal(wrapper.instance().getValue(), "en");
+        radioInput = wrapper.find(".fa-circle-o").last();
+
+        radioInput.simulate("click");
+        wrapper = mount(getComponent({items: langs, value: values, onChange: handleChange}));
+        chai.assert.equal(wrapper.instance().isChecked("en"), true);
+
+    });
 
 
-            radioInput = wrapper.find(".fa-dot-circle-o").first();
-            radioInput.simulate("click");
-            wrapper = mount(getComponent({ items: langs, value: values }));
-            chai.assert.equal(wrapper.instance().getValue(), "en");
+    it("getValue", () => {
+        values = [];
 
-        });
+        let wrapper = mount(getComponent({items: langs, value: values, onChange: handleChange}));
+
+        chai.assert.equal(wrapper.instance().getValue(), "");
+        let radioInput = wrapper.find(".fa-circle-o").first();
+
+        radioInput.simulate("click");
+        wrapper = mount(getComponent({items: langs, value: values}));
+        chai.assert.equal(wrapper.instance().getValue(), "en");
+
+
+        radioInput = wrapper.find(".fa-dot-circle-o").first();
+        radioInput.simulate("click");
+        wrapper = mount(getComponent({items: langs, value: values}));
+        chai.assert.equal(wrapper.instance().getValue(), "en");
+
+    });
 
 });
