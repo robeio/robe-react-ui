31bc745df17df86c5aaf89146f8d21ebc0b14fd2
serayuzgur <serayuzgur@gmail.com>
2016-09-01 15:51:20 +0300

    Test code refactored and cleaned up

--- __test__/inputs/DecimalInput.spec.js
+++ __test__/inputs/DecimalInput.spec.js
@@ -1,46 +1,41 @@
 import chai from "chai";// eslint-disable-line import/no-extraneous-dependencies
-import React from "react";
-import ReactDOM from "react-dom";
-import TestUtils from "react-addons-test-utils";// eslint-disable-line import/no-extraneous-dependencies
+import TestUtils from "../TestUtils";// eslint-disable-line import/no-extraneous-dependencies
 import DecimalInput from "inputs/DecimalInput";// eslint-disable-line
 
 describe("inputs/DecimalInput", () => {
-    const getComponent = (props: Object): Object => {
-        return (
-            <DecimalInput // eslint-disable-line react/jsx-filename-extension
-                label="Label"
-                value={props.value !== undefined ? props.value : "42"}
-                onChange={props.onChange !== undefined ? props.onChange : () => { }}
-                validations={{
-                    required: (value: any): Array => {
-                        return (value === undefined || value === null || value === "") ? "Not Valid" : undefined;
-                    }
-                }}
-            />
-        );
+    const props = {
+        label: "Label",
+        value: "42",
+        onChange: () => { },
+        validations: {
+            required: (value: any): Array => {
+                return (value === undefined || value === null || value === "") ? "Not Valid" : undefined;
+            },
+            requiredMessage: "Error Test"
+        }
     };
-
+   
     it("'props' Controls", () => {
-        let componentNode = TestUtils.renderIntoDocument(getComponent({}));
-        chai.assert.equal(componentNode.props.label, "Label");
-        chai.assert.equal(componentNode.props.value, "42");
-        chai.assert.equal(componentNode.props.onChange.name, "");
-        chai.assert.isDefined(componentNode.props.validations.required, "Validation prop error");
+        let componentNode = TestUtils.mount({}, DecimalInput, props);
+        chai.assert.equal(componentNode.prop("label"), "Label");
+        chai.assert.equal(componentNode.prop("value"), "42");
+        chai.assert.equal(componentNode.prop("onChange").name, "onChange");
+        chai.assert.isDefined(componentNode.prop("validations"), "Validation prop error");
     });
 
     it("'validations' Control", () => {
-        let componentNode = TestUtils.renderIntoDocument(getComponent({}));
-        chai.assert.isOk(componentNode.isValid(), "Non-empty string mus be valid");
-        chai.assert.equal(ReactDOM.findDOMNode(componentNode).getElementsByClassName("input-alert").length, 0, "Non-Empty string value must render ZERO alert");// eslint-disable-line react/no-find-dom-node
+        let componentNode = TestUtils.mount({}, DecimalInput, props);
+        chai.assert.isOk(componentNode.instance().isValid(), "Non-empty string mus be valid");
+        chai.assert.equal(componentNode.find(".input-alert").length, 0, "Non-Empty string value must render ZERO alert");// eslint-disable-line react/no-find-dom-node
         // Must be invalid
 
-        componentNode = TestUtils.renderIntoDocument(getComponent({ value: "" }));
-        chai.assert.isNotOk(componentNode.isValid(), "Empty string must be invalid");
-        chai.assert.equal(ReactDOM.findDOMNode(componentNode).getElementsByClassName("input-alert").length, 1, "Empty string value must render one alert");// eslint-disable-line react/no-find-dom-node
+        componentNode = TestUtils.mount({ value: "" }, DecimalInput, props);
+        chai.assert.isNotOk(componentNode.instance().isValid(), "Empty string must be invalid");
+        chai.assert.equal(componentNode.find(".input-alert").length, 1, "Empty string value must render one alert");// eslint-disable-line react/no-find-dom-node
     });
 
     it("'__numericFilter", (done: Function) => {
-        let componentNode = TestUtils.renderIntoDocument(getComponent({ onChange: () => { return true; } }));
+        let componentNode = TestUtils.mount({ onChange: (): boolean => { return true; } }, DecimalInput, props);
         let e = {
             target: {
                 value: "12"
@@ -53,28 +48,28 @@ describe("inputs/DecimalInput", () => {
             }
         };
         /* eslint-disable no-underscore-dangle */
-        componentNode.__numericFilter(e);
+        componentNode.instance().__numericFilter(e);
         e.target.value = "12.2";
         e.preventDefault = () => {
             chai.assert.isOk(false, "Input value '12.2' failed");
             done("Input value '12.2' failed");
         };
-        componentNode.__numericFilter(e);
+        componentNode.instance().__numericFilter(e);
 
-        componentNode = TestUtils.renderIntoDocument(getComponent({
+        componentNode = TestUtils.mount({
             onChange: () => {
                 chai.assert.isOk(false);
                 done("Input value '12q2' failed");
             }
-        }));
+        }, DecimalInput, props);
         e.target.value = "12q2";
         e.preventDefault = () => {
             chai.assert.isOk(true, "Input value '12q2' failed");
         };
-        componentNode.__numericFilter(e);
+        componentNode.instance().__numericFilter(e);
 
         e.target.value = null;
-        componentNode.__numericFilter(e);
+        componentNode.instance().__numericFilter(e);
         done();
     });
 });
