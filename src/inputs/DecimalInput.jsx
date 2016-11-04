99da4bdc26f7cb3a3cb0735dfce509df2c7eb5d6
serayuzgur <serayuzgur@gmail.com>
2016-11-04 14:01:01 +0200

    * Deleting first number before the decimalSeparator puts 0. * If you enter a value again it clears pre zeros * Showcase update

--- src/inputs/DecimalInput.jsx
+++ src/inputs/DecimalInput.jsx
@@ -65,6 +65,8 @@ export default class DecimalInput extends ShallowComponent {
 
     static refName = "innerInput";
 
+    __caretPosition;
+
     render(): Object {
         /* eslint-disable no-unused-vars */
 
@@ -76,7 +78,7 @@ export default class DecimalInput extends ShallowComponent {
             step={this.props.step}
             value={this.props.value}
             onChange={this.__numericFilter}
-        />);
+            />);
     }
 
     /**
@@ -92,6 +94,17 @@ export default class DecimalInput extends ShallowComponent {
      */
     __numericFilter(e: Object): boolean {
         let value = e.target.value;
+        this.__caretPosition = this.refs[DecimalInput.refName].getCaretPosition();
+        console.log(this.__caretPosition);
+        if (value.indexOf(this.props.decimalSeparator) === 0) {
+            value = `0${value}`;
+            this.__caretPosition++;
+        } else if (value.charAt(0) === "0" && value.indexOf(this.props.decimalSeparator) !== 1) {
+            value = value.substring(1);
+            this.__caretPosition--;
+        }
+
+
         let result = this.__isFloat(value) || value === "";
         if (result && this.props.onChange) {
             e.target.parsedValue = value;
@@ -100,6 +113,9 @@ export default class DecimalInput extends ShallowComponent {
         if (!result) {
             e.preventDefault();
             e.stopPropagation();
+        }
+        if (this.refs[DecimalInput.refName].isFocused()) {
+            this.refs[DecimalInput.refName].setCaretPosition(this.__caretPosition);
         }
         return result;
     }
@@ -110,5 +126,12 @@ export default class DecimalInput extends ShallowComponent {
         }
         let found = input.match(`^[0-9]{1,6}((\\${this.props.decimalSeparator})|(\\${this.props.decimalSeparator}\\d{1,2}))?$`);
         return found !== undefined && found !== null;
+    }
+
+    componentDidUpdate() {
+        if (this.refs[DecimalInput.refName].isFocused()) {
+            console.log(this.__caretPosition);
+            this.refs[DecimalInput.refName].setCaretPosition(this.__caretPosition);
+        }
     }
 }
