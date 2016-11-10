f8ca997a44a16c92f32a19702653c2c5cb4f4142
hasanmumin <hasanmuminn@gmail.com>
2016-11-10 16:31:31 +0300

    DataForm Cascading sample added.

--- site/samples/form/DataFormSample.jsx
+++ site/samples/form/DataFormSample.jsx
@@ -18,8 +18,67 @@ const remote = {
         type: "DELETE"
     }
 };
+const ilce = {
+    34: [
+        {
+            value: "1",
+            text: "Kadıköy"
+        },
+        {
+            value: "2",
+            text: "Bakırköy"
+        },
+        {
+            value: "3",
+            text: "Ataşehir"
+        }
+    ],
+    21: [
+        {
+            value: "4",
+            text: "Ergani"
+        },
+        {
+            value: "5",
+            text: "Bağlar"
+        },
+        {
+            value: "6",
+            text: "Ofis"
+        }
+    ],
+    6: [
+        {
+            value: "7",
+            text: "Çankaya"
+        },
+        {
+            value: "8",
+            text: "Gölbaşı"
+        },
+        {
+            value: "9",
+            text: "Keçiören"
+        }
+    ]
+};
 
 export default class DataFormSample extends ShallowComponent {
+
+    constructor(props: Object) {
+        super(props);
+        this.state = {
+            propsOfFields: {
+                files: {
+                    remote: remote,
+                    onCellRender: this.onCellRender
+                },
+                ilce: {
+                    items: [],
+                    value: undefined
+                }
+            } };
+    }
 
     render(): Object {
         return (
@@ -28,15 +87,28 @@ export default class DataFormSample extends ShallowComponent {
                     header="Example Data Form Label"
                     fields={fields}
                     columnsSize={2}
-                    propsOfFields={{
-                        files: {
-                            remote: remote,
-                            onCellRender: this.onCellRender
-                        }
-                    }}
+                    onChange={this.onChange}
+                    propsOfFields={this.state.propsOfFields}
                 />
             </div>
         );
+    }
+
+    onChange(name: string, e: Object): boolean {
+        if (name === "il") {
+            let value = e.target.value;
+            let state = this.state;
+            if (value) {
+                let items = ilce[value];
+                state.propsOfFields.ilce.items = items;
+            } else {
+                state.propsOfFields.ilce.items = [];
+                state.propsOfFields.ilce.value = undefined;
+            }
+            this.setState(state);
+        }
+
+        return true;
     }
 
 }
