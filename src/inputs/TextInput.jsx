361e883f126ee357ef9d8b566072b88a8a4075ce
serayuzgur <serayuzgur@gmail.com>
2017-01-20 17:46:20 +0300

    onChange focus bug fixed

--- src/inputs/TextInput.jsx
+++ src/inputs/TextInput.jsx
@@ -117,7 +117,7 @@ export default class TextInput extends ShallowComponent {
             e.preventDefault();
             e.stopPropagation();
         }
-        this.innerComponent.focus = true;
+        this.innerComponent.focus();
         return result;
     }
 }
