3dfb83b268a7df29b2cdf7e12a58e8eff6d7cf24
serayuzgur <serayuzgur@gmail.com>
2017-01-23 15:51:33 +0300

    DecimalInput refacored.

--- src/inputs/DecimalInput.jsx
+++ src/inputs/DecimalInput.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ShallowComponent from "robe-react-commons/lib/components/ShallowComponent";
-import {InputGroup} from "react-bootstrap";
+import { InputGroup } from "react-bootstrap";
 import Input from "./BaseInput";
 import FaIcon from "../faicon/FaIcon";
 
@@ -76,7 +76,7 @@ export default class DecimalInput extends ShallowComponent {
     static defaultProps = {
         decimalSeparator: ".",
         value: "",
-        step:1,
+        step: 1,
         disabled: false,
         readOnly: false,
         hidden: false,
@@ -99,19 +99,16 @@ export default class DecimalInput extends ShallowComponent {
                 value={this.props.value}
                 onChange={this.__numericFilter}
                 onKeyDown={this.__handleKeyPress}
-                inputGroupRight={<InputGroup.Addon style={{padding:0}}><div className="col-sm-12">
-                    <div className="col-sm-6">
-                        <span style={{cursor:"pointer"}} onClick ={(e)=>{
-                            this.__valueIncAndDec(e,"inc");
-                        }} >&#x25B2;</span>
-                    </div>
-                    <div className="col-sm-6">
-                       <span style={{cursor:"pointer"}} onClick ={(e)=>{
-                            this.__valueIncAndDec(e,"dec");
-                        }} >&#x25BC;</span>
-                    </div>
-                </div></InputGroup.Addon>}
-            />
+                inputGroupRight={<InputGroup.Addon style={{ padding: 0 }}>
+                    <div style={{ width: "42px" }}>
+                        <div>
+                            <FaIcon name="increment" code="fa-caret-up" style={{ cursor: "pointer" }} onClick={this.__valueIncAndDec} />
+                        </div>
+                        <div>
+                            <FaIcon name="decrement" code="fa-caret-down" style={{ cursor: "pointer" }} onClick={this.__valueIncAndDec} />
+                        </div>
+                    </div></InputGroup.Addon>}
+                />
         );
     }
 
@@ -165,10 +162,10 @@ export default class DecimalInput extends ShallowComponent {
     }
 
     __handleKeyPress = (event) => {
-        if(event.key==="ArrowUp"){
-            this.__valueIncAndDec(event,"inc");
-        }else if(event.key==="ArrowDown"){
-            this.__valueIncAndDec(event,"dec");
+        if (event.key === "ArrowUp") {
+            this.__valueIncAndDec(event, "inc");
+        } else if (event.key === "ArrowDown") {
+            this.__valueIncAndDec(event, "dec");
         }
     }
 
@@ -179,18 +176,18 @@ export default class DecimalInput extends ShallowComponent {
         let found = input.match(`^[0-9]{1,6}((\\${this.props.decimalSeparator})|(\\${this.props.decimalSeparator}\\d{1,2}))?$`);
         return found !== undefined && found !== null;
     }
-    __valueIncAndDec(e,type){
-        e.target = {};
-        e.target.name=this.props.name;
+    __valueIncAndDec(e) {
+        let type = e.target.getAttribute("name");
+        e.target.name = this.props.name;
 
-        let currentValue = parseFloat(this.props.value,10);
+        let currentValue = parseFloat(this.props.value, 10);
 
-        if(type=="inc"){
-            e.target.value = currentValue?(currentValue+this.props.step):(this.props.step);
-        }else {
-            e.target.value = currentValue?(currentValue-this.props.step):"";
+        if (type == "increment") {
+            e.target.value = currentValue ? (currentValue + this.props.step) : (this.props.step);
+        } else {
+            e.target.value = currentValue ? (currentValue - this.props.step) : "";
         }
-        e.target.value+="";
+        e.target.value += "";
         this.__numericFilter(e);
     }
 
